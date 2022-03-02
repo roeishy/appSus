4d4c0bd51346f12bd3d26bf2457fabe4e78516f3
FaresMulla <farismolla888@gmail.com>
2022-03-02 17:28:06 +0200

    header

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,3 +1,4 @@
+import { userService } from '../apps/login/services/userService.js'
 export default {
     template: `
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -9,16 +10,16 @@ export default {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                    <router-link to="/home/:userId"><button class="btn btn-dark btn-outline-warning m-1 text-light">Home</button></router-link>
+                    <router-link to="/home/user.id"><button class="btn btn-dark btn-outline-warning m-1 text-light">Home</button></router-link>
                     </li>
                     <li class="nav-item">
                     <router-link to="/about"><button class="btn btn-dark btn-outline-warning m-1 text-light">About</button></router-link>
                     </li>
                     <li class="nav-item">
-                    <router-link to="/mail/"><button class="btn btn-dark btn-outline-warning m-1 text-light">Mails</button></router-link>
+                    <router-link to="/mail/+user.id"><button class="btn btn-dark btn-outline-warning m-1 text-light">Mails</button></router-link>
                     </li> 
                     <li class="nav-item">
-                    <router-link to="/keeps/"><button class="btn btn-dark btn-outline-warning m-1 text-light">Keeps</button></router-link>
+                    <router-link to="/keeps/+user.id"><button class="btn btn-dark btn-outline-warning m-1 text-light">Keeps</button></router-link>
                     </li>                                        
                   </ul>  
                   <div class="float-end">                  
@@ -27,8 +28,31 @@ export default {
                 </div>
               </div>
             </nav>
-
+            <!-- <pre>{{user}}</pre> -->
         
     
-    `
+    `,
+    data(){
+        return{
+            id: null,
+            user: null,
+            users: null,
+        }
+    },
+    created() {
+        this.id = this.$route.params.userId;
+        userService.getLogedUser(this.id)
+            .then(user => {
+                this.user = user
+            })
+    },
+    methods: {
+    },
+    computed: {
+        allUsers() {
+            return userService.query()
+                .then(users => this.users = users);
+        }        
+        
+    }
 }
