d6e371983c27e50d6b60d70da726e31a9471266d
FaresMulla <farismolla888@gmail.com>
2022-03-06 08:37:42 +0200

    signup page

--- js/apps/login/cmps/sign-up-page.cmp.js
+++ js/apps/login/cmps/sign-up-page.cmp.js
@@ -1,8 +1,27 @@
 export default {
     props: ['users'],
     template: `
-        <section class="home-page app-main">
-            <h3>sign up</h3>
+        <section class="home-page app-main d-flex justify-content-center align-items-center">
+        <div class="card" style="width: 30%;">
+            <h1>sign up</h1>
+          <img src="https://icon-library.com/images/avatar-icon-png/avatar-icon-png-8.jpg" style="width:300px" class="card-img-top rounded mx-auto" alt="...">
+          <div class="card-body">            
+          <form @submit.prevent="login" autocomplete="off">
+                <label for="uName" class="col-12 fs-5">
+                    user name:
+                    <input class="form-control" placeholder="User Name" v-model="user.userName" type="text" id="uName" required>
+                </label>
+                <label for="pass" class="col-12 fs-5">
+                    password:
+                    <input class="form-control" placeholder="Password"  v-model="user.pass" type="password" id="pass" required>
+                </label>
+                <button class="btn btn-primary col-12 mt-3">Send</button>                
+            </form>
+            
+          </div>
+        </div>  
+
+            <!-- <h3>sign up</h3>
             <form @submit.prevent="signUp">
                 <label for="uName">
                     name:
@@ -15,7 +34,7 @@ export default {
                 <button >Send</button>
             </form>
             
-        </section>
+        </section> -->
     `,
     data() {
         return {
